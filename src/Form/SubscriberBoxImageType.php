fa01b434ff15b0c5787b18833a6be36c9d2cbf5e
Cristóbal Cobos Budia <cristobal.cobos@intnova.com>
2020-02-24 14:07:02 +0100

    Fixed: Fallo al mostrar previsualización de imágenes VichUploader en formularios.

--- src/Form/SubscriberBoxImageType.php
+++ src/Form/SubscriberBoxImageType.php
@@ -18,7 +18,6 @@ class SubscriberBoxImageType extends AbstractType
                     'allow_delete' => false,
                     'download_uri' => false,
                     'label' => 'Imagen',
-//                    'imagine_pattern' => 'height_1080',
                 ])
         ;
     }
