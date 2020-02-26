b1d31f000361ece7a426a5f12920b080715e7212
Cristóbal Cobos Budia <cristobal.cobos@intnova.com>
2020-02-26 13:08:20 +0100

    Quitar color de cable al mostrar pasantes en torpedos y mover icono de eliminar fusiones a la derecha CSS.

--- public/js/app/element/builder/PopupBuilder.js
+++ public/js/app/element/builder/PopupBuilder.js
@@ -255,7 +255,6 @@ var PopupBuilder = (function () {
                             if (passant.fibers[0] && passant.fibers[1]) {
                                 html += "<li>";
                                 html += "<span style='position:relative;top:-10px;'>" + passant.fibers[0].tube.wire.name + " </span>";
-                                html += "<div class='fusion wire-left' title='" + passant.fibers[0].tube.wire.name + "' style='background-color:" + passant.fibers[0].tube.wire.hexaColor + "'></div>";
                                 html += "<div class='fusion tube-left' style='background-color:" + passant.fibers[0].tube.hexaColor + "' title='Layer " + passant.fibers[0].tube.layer + "'></div>";
                                 html += "<div class='fusion fiber-left' style='background-color:" + passant.fibers[0].hexaColor + "' title='Layer " + passant.fibers[0].tube.layer + "'></div>";
                                 html += "<div class='fusion signal-loss'>";
@@ -263,7 +262,6 @@ var PopupBuilder = (function () {
                                 html += "</div>";
                                 html += "<div class='fusion fiber-right' style='background-color:" + passant.fibers[1].hexaColor + "' title='Layer " + passant.fibers[1].tube.layer + "'></div>";
                                 html += "<div class='fusion tube-right' style='background-color:" + passant.fibers[1].tube.hexaColor + "' title='Layer " + passant.fibers[1].tube.layer + "'></div>";
-                                html += "<div class='fusion wire-right' title='" + passant.fibers[1].tube.wire.name + "' style='background-color:" + passant.fibers[1].tube.wire.hexaColor + "'></div>";
                                 html += "<span style='position:relative;top:-10px;'> " + passant.fibers[1].tube.wire.name + "</span>";
                                 html += "</li>";
                             }
