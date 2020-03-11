e8603a92e116eebccfc5bf6d36cbb2e8553c3a62
Cristóbal Cobos Budia <cristobal.cobos@intnova.com>
2020-03-11 09:38:44 +0100

    Añadir ROMPathBuilder para la creación de trayectorias desde la ROM

---
+++ public/js/app/element/builder/ROMPathBuilder.js
@@ -0,0 +1,214 @@
+/* global Path, AttributeEnum, ApiUrl, AjaxAdapter */
+
+/**
+ * @return {ROMPathBuilder.PathBuilderAnonym$4}
+ */
+var ROMPathBuilder = (function () {
+
+    /**
+     * @type {ROMPathBuilder}
+     */
+    var instance;
+
+    /**
+     * @type {ROMPathBuilder}
+     */
+    var self = this;
+
+    /**
+     * @type {string}
+     */
+    var html = "<div id='tree'>";
+
+
+    function init() {
+
+
+
+        return {
+
+            /**
+             * @param {{ latiguilloPath, fiber}} patchPanelConector
+             * @returns {ROMPathBuilder}
+             */
+            addOltPath: async function (patchPanelConector) {
+                if (patchPanelConector.latiguilloPatch) {
+                    var latiguilloPatch = patchPanelConector.latiguilloPatch;
+                    html += "<ul>";
+                    html += "<li>";
+                    html += "<strong>Trayectoria a OLT</strong>";
+                    html += "<ul>";
+                    html += "<li>";
+                    html += "Latiguillo a EDFA: " + latiguilloPatch.name;
+                    if (latiguilloPatch.edfaPort) {
+                        var edfaPort = latiguilloPatch.edfaPort;
+                        html += "<ul>";
+                        html += "<li>";
+                        html += "Puerto EDFA: " + edfaPort.edfaSlot.edfa.name + " Puerto <strong>" + edfaPort.number + "</strong>";
+                        await AjaxAdapter.get(ApiUrl.GET_EDFA_PORT_ID + edfaPort.id + "/" + ((edfaPort.edfaSlot.type.toLowerCase() === 'out') ? 'in' : 'out')).then(function (response) {
+                            var edfaPort = response.data;
+                            if (edfaPort.latiguilloEdfa) {
+                                var latiguilloEdfa = edfaPort.latiguilloEdfa;
+                                html += "<ul>";
+                                html += "<li>";
+                                html += "Latiguillo a OLT: " + latiguilloEdfa.name;
+                                if (latiguilloEdfa.oltPort) {
+                                    var oltPort = latiguilloEdfa.oltPort;
+                                    html += "<ul>";
+                                    html += "<li>";
+                                    html += "Puerto OLT: <strong>" + oltPort.oltSlot.olt.name + " / Tarjeta " + oltPort.oltSlot.number + " / Puerto " + oltPort.number + "</strong>";
+                                    html += "</li>";
+                                    html += "</ul>";
+                                }
+                                html += "</li>";
+                                html += "</ul>";
+                            }
+                        });
+
+                        html += "</li>";
+                        html += "</ul>";
+                    }
+                    html += "</li>";
+                    html += "</ul>";
+                    html += "</li>";
+                    html += "</ul>";
+                }
+
+                return this;
+            },
+
+            /**
+             * 
+             * @param {{id:Number, tube, hexaColor:string, torpedoFusions:[], torpedoPassans:[], distributionBoxPassants}} fiber
+             * @returns {ROMPathBuilderROMPathBuilder.init.ROMPathBuilderAnonym$0}
+             */
+            addDistributionBoxPath: function (fiber) {
+                var self = instance;
+                if (fiber) {
+                    html += "<ul>";
+                    html += "<li>";
+                    html += "<strong>Trayectoria a Caja Distribución</strong>";
+                    html += self.generateNexo(fiber);
+                    html += "</li>";
+                    html += "</ul>";
+                }
+
+                return this;
+            },
+
+            /**
+             * 
+             * @param {{id:Number, color:string, distributionBoxPort:DistributionBoxPort, hexaColor:string, patchPanelSlotConector:PatchPanelSlotConector, torpedoFusions:array, torpedoPassants:array, tube:Tube}} fiber
+             * @returns {undefined}
+             */
+            generateNexo: function (fiber) {
+                var self = instance;
+                var html = "<ul>";
+                html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + fiber.tube.wire.image + "\"}'>";
+                html += "Fibra: " + fiber.tube.wire.name + " <strong>(" + fiber.tube.wire.longitude.toFixed(2) + "m)</strong>";
+                html += "<div class='wire tube-left' style='background-color:" + fiber.tube.hexaColor + "'></div>";
+                html += "<div class='wire fiber-left' style='background-color:" + fiber.hexaColor + "'></div>";
+
+                fiber.torpedoFusions.forEach(function (fusion) {
+                    if (fusion) {
+                        html += "<ul>";
+                        html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + fusion.torpedo.icon + "\"}'>";
+                        html += "Fusión en torpedo: " + fusion.torpedo.name;
+                        if (fusion.torpedo.address) {
+                            html += " <strong>(" + fusion.torpedo.address.location + ")</strong>";
+                        }
+                        fusion.fibers.forEach(function (iFiber) {
+                            if (iFiber) {
+                                html += self.generateNexo(iFiber);
+                            }
+                        });
+                        html += "</li>";
+                        html += "</ul>";
+                    }
+                });
+
+                fiber.torpedoPassants.forEach(function (passant) {
+                    if (passant) {
+                        html += "<ul>";
+                        html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + passant.torpedo.icon + "\"}'>";
+                        html += "Pasante en torpedo: " + passant.torpedo.name;
+                        if (passant.torpedo.address) {
+                            html += " <strong>(" + passant.torpedo.address.location + ")</strong>";
+                        }
+                        passant.fibers.forEach(function (iFiber) {
+                            if (iFiber) {
+                                html += self.generateNexo(iFiber);
+                            }
+                        });
+                        html += "</li>";
+                        html += "</ul>";
+                    }
+                });
+
+                fiber.distributionBoxPassants.forEach(function (passant) {
+                    if (passant) {
+                        html += "<ul>";
+                        html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + passant.distributionBox.icon + "\"}'>";
+                        html += "Pasante en caja distribución: " + passant.distributionBox.name;
+                        if (passant.distributionBox.address) {
+                            html += " <strong>(" + passant.distributionBox.address.location + ")</strong>";
+                        }
+                        passant.fibers.forEach(function (iFiber) {
+                            if (iFiber) {
+                                html += self.generateNexo(iFiber);
+                            }
+                        });
+                        html += "</li>";
+                        html += "</ul>";
+                    }
+                });
+
+                if (fiber.patchPanelSlotConector) {
+                    html += "<ul>";
+                    html += "<li>";
+                    html += "Conector CPD: ";
+                    html += fiber.patchPanelSlotConector.patchPanelSlot.patchPanel.rack.name + "_";
+                    html += fiber.patchPanelSlotConector.patchPanelSlot.patchPanel.name + "_";
+                    html += fiber.patchPanelSlotConector.patchPanelSlot.number + "_";
+                    html += fiber.patchPanelSlotConector.number;
+                    html += " <strong>(" + fiber.patchPanelSlotConector.patchPanelSlot.patchPanel.rack.name + ")</strong>";
+                    html += "</li>";
+                    html += "</ul>";
+                }
+
+                if (fiber.distributionBoxPort) {
+                    html += "<ul>";
+                    html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + fiber.distributionBoxPort.distributionBox.icon + "\"}'>";
+                    html += "Caja distribución: " + fiber.distributionBoxPort.distributionBox.name + " <strong>(" + fiber.distributionBoxPort.distributionBox.address.location + ")</strong>";
+                    html += "</li>";
+                    html += "</ul>";
+                }
+
+                html += "</li>";
+                html += "</ul>";
+
+                return html;
+            },
+
+            /**
+             * @return {string}
+             */
+            build: function () {
+                html += "</div>";
+                return html;
+            }
+        };
+
+    }
+    ;
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = init();
+            }
+            return instance;
+        }
+    };
+
+})();
--- public/js/app/element/listener/PatchPanelListener.js
+++ public/js/app/element/listener/PatchPanelListener.js
@@ -1,4 +1,4 @@
-/* global Path, ApiUrl, AjaxAdapter, self, ModalAdapter, TreeAdapter, AlertAdapter */
+/* global Path, ApiUrl, AjaxAdapter, self, ModalAdapter, TreeAdapter, AlertAdapter, ROMPathBuilder */
 
 /**
  * @type PatchPanelListener
@@ -10,166 +10,21 @@ var PatchPanelListener = {
      * @returns {undefined}
      */
     showModal: function (patchPanelConectorId) {
-        var self = this;
-
         AjaxAdapter.get(ApiUrl.GET_CONECTOR_ID + patchPanelConectorId).then(async function (response) {
             var patchPanelConector = response.data;
-//            console.log(patchPanelConector);
-            var html = "";
-            if (patchPanelConector.latiguilloPatch) {
-                var latiguilloPatch = patchPanelConector.latiguilloPatch;
-                html += "<div id='tree'>";
-                html += "<ul>";
-                html += "<li>";
-                html += "<strong>Trayectoria a OLT</strong>";
-                html += "<ul>";
-                html += "<li>";
-                html += "Latiguillo a EDFA: " + latiguilloPatch.name;
-                if (latiguilloPatch.edfaPort) {
-                    var edfaPort = latiguilloPatch.edfaPort;
-                    html += "<ul>";
-                    html += "<li>";
-                    html += "Puerto EDFA: " + edfaPort.edfaSlot.edfa.name + " (" + edfaPort.edfaSlot.type + ") <strong>" + edfaPort.number + "</strong>";
-                    await AjaxAdapter.get(ApiUrl.GET_EDFA_PORT_ID + edfaPort.id + "/in").then(function (response) { // todo: IN
-                        var edfaPort = response.data;
-                        if (edfaPort.latiguilloEdfa) {
-                            var latiguilloEdfa = edfaPort.latiguilloEdfa;
-                            html += "<ul>";
-                            html += "<li>";
-                            html += "Latiguillo a OLT: " + latiguilloEdfa.name;
-                            if (latiguilloEdfa.oltPort) {
-                                var oltPort = latiguilloEdfa.oltPort;
-                                html += "<ul>";
-                                html += "<li>";
-                                html += "Puerto OLT: <strong>" + oltPort.oltSlot.olt.name + " / Tarjeta " + oltPort.oltSlot.number + " / Puerto " + oltPort.number + "</strong>";
-                                html += "</li>";
-                                html += "</ul>"
-                            }
-                            html += "</li>";
-                            html += "</ul>"
-                        }
-                    });
-
-                    html += "</li>";
-                    html += "</ul>"
-                }
-                html += "</li>";
-                html += "</ul>"
-                html += "</li>";
-                html += "</ul>";
-            } else {
-                html += "<div id='tree'>";
-            }
             var conectorFiber = patchPanelConector.fiber;
-            if (conectorFiber) {
-                html += "<ul>";
-                html += "<li>";
-                html += "<strong>Trayectoria a Caja Distribución</strong>";
-                html += self.generateNexo(conectorFiber);
-                html += "</li>";
-                html += "</ul>";
-                html += "</div>";
-            }
+            
+            var romPathBuilder = ROMPathBuilder.getInstance();
+            await romPathBuilder.addOltPath(patchPanelConector);
+            romPathBuilder.addDistributionBoxPath(conectorFiber);
+//            console.log(patchPanelConector);
+            
 
-            ModalAdapter.showModal('Trayectoria', html);
+            ModalAdapter.showModal('Trayectoria', romPathBuilder.build());
             TreeAdapter.showTree('tree');
         });
 
         return false;
-    },
-
-    /**
-     * 
-     * @param {{id:Number, color:string, distributionBoxPort:DistributionBoxPort, hexaColor:string, patchPanelSlotConector:PatchPanelSlotConector, torpedoFusions:array, torpedoPassants:array, tube:Tube}} fiber
-     * @returns {undefined}
-     */
-    generateNexo: function (fiber) {
-        var self = this;
-        var html = "<ul>";
-        html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + fiber.tube.wire.image + "\"}'>";
-        html += "Fibra: " + fiber.tube.wire.name + " <strong>(" + fiber.tube.wire.longitude.toFixed(2) + "m)</strong>";
-        html += "<div class='wire tube-left' style='background-color:" + fiber.tube.hexaColor + "'></div>";
-        html += "<div class='wire fiber-left' style='background-color:" + fiber.hexaColor + "'></div>";
-
-        fiber.torpedoFusions.forEach(function (fusion) {
-            if (fusion) {
-                html += "<ul>";
-                html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + fusion.torpedo.icon + "\"}'>";
-                html += "Fusión en torpedo: " + fusion.torpedo.name;
-                if (fusion.torpedo.address) {
-                    html += " <strong>(" + fusion.torpedo.address.location + ")</strong>";
-                }
-                fusion.fibers.forEach(function (iFiber) {
-                    if (iFiber) {
-                        html += self.generateNexo(iFiber);
-                    }
-                });
-                html += "</li>";
-                html += "</ul>";
-            }
-        });
-
-        fiber.torpedoPassants.forEach(function (passant) {
-            if (passant) {
-                html += "<ul>";
-                html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + passant.torpedo.icon + "\"}'>";
-                html += "Pasante en torpedo: " + passant.torpedo.name;
-                if (passant.torpedo.address) {
-                    html += " <strong>(" + passant.torpedo.address.location + ")</strong>";
-                }
-                passant.fibers.forEach(function (iFiber) {
-                    if (iFiber) {
-                        html += self.generateNexo(iFiber);
-                    }
-                });
-                html += "</li>";
-                html += "</ul>";
-            }
-        });
-
-        fiber.distributionBoxPassants.forEach(function (passant) {
-            if (passant) {
-                html += "<ul>";
-                html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + passant.distributionBox.icon + "\"}'>";
-                html += "Pasante en caja distribución: " + passant.distributionBox.name;
-                if (passant.distributionBox.address) {
-                    html += " <strong>(" + passant.distributionBox.address.location + ")</strong>";
-                }
-                passant.fibers.forEach(function (iFiber) {
-                    if (iFiber) {
-                        html += self.generateNexo(iFiber);
-                    }
-                });
-                html += "</li>";
-                html += "</ul>";
-            }
-        });
-
-        if (fiber.patchPanelSlotConector) {
-            html += "<ul>";
-            html += "<li>";
-            html += "Conector CPD: ";
-            html += fiber.patchPanelSlotConector.patchPanelSlot.patchPanel.rack.name + "_";
-            html += fiber.patchPanelSlotConector.patchPanelSlot.patchPanel.name + "_";
-            html += fiber.patchPanelSlotConector.patchPanelSlot.number + "_";
-            html += fiber.patchPanelSlotConector.number;
-            html += " <strong>(" + fiber.patchPanelSlotConector.patchPanelSlot.patchPanel.rack.name + ")</strong>";
-            html += "</li>";
-            html += "</ul>";
-        }
-
-        if (fiber.distributionBoxPort) {
-            html += "<ul>";
-            html += "<li data-jstree='{\"icon\":\"" + Path.IMAGE_UPLOADS + fiber.distributionBoxPort.distributionBox.icon + "\"}'>";
-            html += "Caja distribución: " + fiber.distributionBoxPort.distributionBox.name + " <strong>(" + fiber.distributionBoxPort.distributionBox.address.location + ")</strong>";
-            html += "</li>";
-            html += "</ul>";
-        }
-
-        html += "</li>";
-        html += "</ul>";
-
-        return html;
     },
 
     /**
