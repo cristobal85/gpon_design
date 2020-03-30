b3d2e593b935d359e98dd0e456e73600f472f2fb
Cristóbal Cobos Budia <cristobal@MBPdeCristobal.mimosa.co>
2020-03-30 12:09:08 +0200

    UPDATED: Añadido sticky navbar para fijar menu cuado se haga scroll.

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,14 +4,30 @@ $(document).ready(function () {
             "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
         },
         "oSearch": {
-            "bSmart": false, 
+            "bSmart": false,
             "bRegex": true,
-            "sSearch": ""                
+            "sSearch": ""
         }
     });
-    
+
 //  Add and drop multiple entity
     $('.collection-type').collection();
 //  Add select2 library to all select fields
     $('select').select2();
+
+
+    window.onscroll = function () {
+        myFunction()
+    };
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= 80) {
+            navbar.classList.add("fixed-top")
+        } else {
+            navbar.classList.remove("fixed-top");
+        }
+    }
 });
