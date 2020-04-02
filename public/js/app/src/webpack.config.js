169eb5e2967240e043fc5027f79542f76ce33110
Cristóbal Cobos Budia <cristobal@MBPdeCristobal.mimosa.co>
2020-04-02 16:01:45 +0200

    UPDATED: change app folder to src and generate dist file.

---
+++ public/js/app/src/webpack.config.js
@@ -0,0 +1,26 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
+console.log(__dirname + '/../dist/');
+
+module.exports = {
+
+    entry: ["./app.js"],
+    output: {
+        path: __dirname + '/../dist/',
+        filename: "gpon-design.min.js"
+    },
+    plugins: [
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /a\.js|node_modules/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd(),
+        })
+    ]
+
+}
